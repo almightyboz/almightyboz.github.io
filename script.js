a074a7a8d161ed9d1627dad5a97999ed81423a17
Emily <ebosakowski@gmail.com>
2016-08-25 17:12:18 -0500

    Make particles generate in random places, change color and direction on click events. Need to add words to canvas container

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // initialize and empty canvas and place it on the page
 var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 500;
+canvas.width = $(window).width();
+canvas.height = $(window).height;
+
+var colorArray = ['#FFDBFB', '#EFD7FF', '#BDFFEA', '#FAFBDF']
 
 // initial starting position
 var posX = 150;
@@ -13,43 +15,35 @@ $( document ).ready(function(event){
   document.body.appendChild(canvas);
 
 
-  // $('canvas').on("mousemove", function( event ) {
+    var particles = {};
+    var particleIndex = 0;
 
-  //   posX = event.pageX;
-  //   posY = event.pageY;
-
-    // console.log("Position x is "+ posX);
-    // console.log("Position Y is "+ posY);
-
-
-      var particles = {};
-      var particleIndex = 0;
-
-      var settings = {
-        density: 2,
-        particleSize: 1,
-        startingX: posX,
-        startingY: posY,
-        gravity: 0.5,
-        // groundLevel: canvas.height * 0.75,
-        // leftWall: canvas.width * 0.25,
-        // rightWall: canvas.width * 0.75,
-      };
+    var settings = {
+      density: 20,
+      particleSize: 0.5,
+      startingX: Math.random() * canvas.width,
+      startingY: Math.random() * canvas.height,
+      gravity: 0.5,
+      color: "#ffffff"
+      // groundLevel: canvas.height * 0.75,
+      // leftWall: canvas.width * 0.25,
+      // rightWall: canvas.width * 0.75,
+    };
 
     function Particle() {
       // establish starting positions and velocities
-      this.x = settings.startingX;
-      this.y = settings.startingY;
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
       //random X and Y velocities
-      this.vx = Math.random() * 20 - 10;
-      this.vy = Math.random() * 20 - 5;
+      this.vx = Math.random() * 15 - 10;
+      this.vy = Math.random() * 15 - 5;
       // add new particle to the index
       particleIndex ++;
       particles[particleIndex] = this;
       this.id = particleIndex;
       // a counter to use to determine when to have particle vanish
       this.life = 0;
-      this.maxLife = 10;
+      this.maxLife = 100;
     }
 
     Particle.prototype.draw = function() {
@@ -68,38 +62,42 @@ $( document ).ready(function(event){
         delete particles[this.id];
       }
       // create the shapes
-      context.clearRect(0, 0, settings.groundLevel, canvas.width, canvas.height);
+      context.clearRect(settings.leftWall, settings.groundLevel, canvas.width, canvas.height);
       context.beginPath();
-      context.fillStyle="#ffffff";
+      context.fillStyle = settings.color;
       context.arc(this.x, this.y, settings.particleSize, 0, Math.PI*2, true);
       context.closePath();
       context.fill();
     };
 
-    function makeParticles() {
-            // erase canvas
-      context.fillStyle = "black";
-      context.fillRect(0,0, canvas.width, canvas.height);
-      // draw a left and right wall and floor
-      context.fillStyle= "white";
-      context.fillRect(0, 0, settings.leftWall, canvas.height);
-      context.fillRect(settings.rightWall, 0, canvas.width, canvas.height);
-      context.fillRect(0, settings.groundLevel, canvas.width, canvas.height);
-      // generate the particles
-      for (var i = 0; i < settings.density; i++) {
-        if (Math.random() > 0.5) {
-          new Particle();
-        }
-      }
-      //draw each particles
-      for (var j in particles) {
-        particles[j].draw();
+    setInterval(function() {
+          // erase canvas
+    context.fillStyle = "black";
+    context.fillRect(0,0, canvas.width, canvas.height);
+    // generate the particles
+    for (var i = 0; i < settings.density; i++) {
+      if (Math.random() > 0.5) {
+        new Particle();
       }
     }
+    //draw each particles
+    for (var j in particles) {
+      particles[j].draw();
+    }
+    }, 30);
 
-    setInterval(makeParticles(), 30);
+    $('canvas').on('click', function(event) {
 
-  // });
+      var newColor = colorArray[Math.round(Math.random() * 4)];
+      settings.color = newColor;
+
+      if (settings.gravity == 0.5) {
+        settings.gravity = -0.5
+      } else {
+        settings.gravity = 0.5
+      };
+
+    });
 
 
 
